dcf7d7ac5fe4d50ecef94e1be3d4b75bd51c836f
Kristian Hennings <krhe@inventors.dk>
2020-03-11 23:15:01 +0100

    Implemented functions and messages for the CPAR device

---
+++ LabBench.CPAR/LabBench.CPAR/CPARDevice.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Inventors.CPAR.Functions;
+using Inventors.CPAR.Messages;
+using Inventors.ECP;
+using Inventors.ECP.Communication;
+using Inventors.ECP.Functions;
+using Inventors.Logging;
+
+namespace LabBench.CPAR
+{
+    public class CPARDevice :
+        Device
+    {
+        public CPARDevice() :
+            base(new SerialPortLayer())
+        {
+            BaudRate = 38400;
+
+            FunctionList.Add(new DeviceIdentification());
+            FunctionList.Add(new Ping());
+            FunctionList.Add(new GetEndianness());
+
+            FunctionList.Add(new SetWaveformProgram());
+            FunctionList.Add(new StartStimulation());
+            FunctionList.Add(new StopStimulation());
+            FunctionList.Add(new WriteSerialNumber());
+            FunctionList.Add(new WriteCalibration());
+            FunctionList.Add(new ReadCalibration());
+            FunctionList.Add(new ForceStartStimulation());
+
+
+            Master.Add(new StatusMessage());
+            Master.Add(new EventMessage());
+        }
+
+        public override bool IsCompatible(DeviceIdentification identification)
+        {
+            if (identification == null)
+            {
+                throw new ArgumentNullException(nameof(identification));
+            }
+
+            return (identification.ManufactureID == Manufacturer.Nocitech) && 
+                   (identification.DeviceID == 1) &&
+                   (identification.MajorVersion >= 7);
+        }
+
+        public void Accept(EventMessage msg)
+        {
+            Log.Debug(msg.Code.ToString());
+        }
+
+        public void Accept(StatusMessage msg)
+        {
+            Log.Debug("VAS: {0}", msg.VasScore);
+        }
+
+        #region Device Definitions
+        public enum PressureType
+        {
+            SUPPLY_PRESSURE = 0,
+            STIMULATING_PRESSURE
+        }
+
+        public static readonly int UPDATE_RATE = 20;
+        public static readonly double MAX_PRESSURE = 100;
+        public static readonly double MAX_SUPPLY_PRESSURE = 1000;
+        public static readonly double MAX_SCORE = 10;
+
+        public static int TimeToRate(double time)
+        {
+            return (int)Math.Round(time * UPDATE_RATE);
+        }
+
+        public static double BinaryToPressure(byte x, PressureType type = PressureType.STIMULATING_PRESSURE)
+        {
+            if (type == PressureType.SUPPLY_PRESSURE)
+            {
+                return MAX_SUPPLY_PRESSURE * ((double)x) / byte.MaxValue;
+            }
+            else
+            {
+                return MAX_PRESSURE * ((double)x) / byte.MaxValue;
+            }
+        }
+
+        public static double BinaryToScore(byte x)
+        {
+            return MAX_SCORE * ((double)x) / byte.MaxValue;
+        }
+
+        public static double PressureToBinary(double pressure)
+        {
+            return (255.0 / 100.0) * pressure;
+        }
+
+        public static double DeltaPressureToBinary(double delta)
+        {
+            return (255.0 / 100.0) * (delta / UPDATE_RATE);
+        }
+
+        public static double CountToTime(int count)
+        {
+            return ((double)count) / UPDATE_RATE;
+        }
+
+        public static ushort TimeToCount(double time)
+        {
+            return (ushort)Math.Ceiling(time * UPDATE_RATE);
+        }
+
+        public static SetWaveformProgram CreateEmptyProgram(byte channel)
+        {
+            return new SetWaveformProgram()
+            {
+                Channel = channel,
+                Instructions = new SetWaveformProgram.Instruction[] { },
+                Repeat = 1
+            };
+        }
+
+        public static SetWaveformProgram CreateRampProgram(byte channel, double delta, double limit)
+        {
+            return new SetWaveformProgram()
+            {
+                Channel = channel,
+                Instructions = new SetWaveformProgram.Instruction[]
+                {
+                    SetWaveformProgram.CreateIncrementInstr(delta, limit/delta)
+                },
+                Repeat = 1
+            };
+
+        }
+
+        public static SetWaveformProgram CreateConditioningProgram(byte channel,
+                                                                   double DELTA_COND_PRESSURE,
+                                                                   double CONDITIONING_PRESSURE,
+                                                                   double DELTA_PRESSURE,
+                                                                   double PRESSURE_LIMIT)
+        {
+            return new SetWaveformProgram()
+            {
+                Channel = channel,
+                Instructions = new SetWaveformProgram.Instruction[]
+                {
+                    new SetWaveformProgram.Instruction()
+                    {
+                        InstructionType = SetWaveformProgram.InstructionType.INC,
+                        Argument = DeltaPressureToBinary(DELTA_COND_PRESSURE),
+                        Steps = TimeToCount(CONDITIONING_PRESSURE/DELTA_COND_PRESSURE)
+                    },
+                    new SetWaveformProgram.Instruction()
+                    {
+                        InstructionType = SetWaveformProgram.InstructionType.STEP,
+                        Argument = PressureToBinary(CONDITIONING_PRESSURE),
+                        Steps = TimeToCount(PRESSURE_LIMIT/DELTA_PRESSURE)
+                    }
+                },
+            };
+        }
+
+        public static SetWaveformProgram CreateDelayedRampProgram(byte channel,
+                                                                  double DELTA_COND_PRESSURE,
+                                                                  double CONDITIONING_PRESSURE,
+                                                                  double DELTA_PRESSURE,
+                                                                  double PRESSURE_LIMIT)
+        {
+            return new SetWaveformProgram()
+            {
+                Channel = channel,
+                Instructions = new SetWaveformProgram.Instruction[]
+                {
+                    new SetWaveformProgram.Instruction()
+                    {
+                        InstructionType = SetWaveformProgram.InstructionType.STEP,
+                        Argument = 0,
+                        Steps = TimeToCount(CONDITIONING_PRESSURE/DELTA_COND_PRESSURE)
+                    },
+                    new SetWaveformProgram.Instruction()
+                    {
+                        InstructionType = SetWaveformProgram.InstructionType.INC,
+                        Argument = DeltaPressureToBinary(DELTA_PRESSURE),
+                        Steps = TimeToCount(PRESSURE_LIMIT/DELTA_PRESSURE)
+                    }
+                },
+                Repeat = 1
+            };
+        }
+
+        public static SetWaveformProgram CreatePulseProgram(byte channel,
+                                                            byte NO_OF_STIMULI,
+                                                            double T_ON,
+                                                            double T_OFF,
+                                                            double P_STIMULATE,
+                                                            double P_STATIC)
+        {
+            return new SetWaveformProgram()
+            {
+                Channel = channel,
+                Instructions = new SetWaveformProgram.Instruction[]
+                {
+                    new SetWaveformProgram.Instruction()
+                    {
+                        InstructionType = SetWaveformProgram.InstructionType.STEP,
+                        Argument = PressureToBinary(P_STIMULATE),
+                        Steps = TimeToCount(T_ON)
+                    },
+                    new SetWaveformProgram.Instruction()
+                    {
+                        InstructionType = SetWaveformProgram.InstructionType.STEP,
+                        Argument = PressureToBinary(P_STATIC),
+                        Steps = TimeToCount(T_OFF)
+                    }
+                },
+                Repeat = NO_OF_STIMULI
+            };
+        }
+
+        public static SetWaveformProgram CreateStaticProgram(byte channel,
+                                                            double t,
+                                                            double p)
+        {
+            return new SetWaveformProgram()
+            {
+                Channel = channel,
+                Instructions = new SetWaveformProgram.Instruction[]
+                {
+                    new SetWaveformProgram.Instruction()
+                    {
+                        InstructionType = SetWaveformProgram.InstructionType.STEP,
+                        Argument = PressureToBinary(p),
+                        Steps = TimeToCount(t)
+                    }
+                },
+                Repeat = 1
+            };
+        }
+
+        public static SetWaveformProgram CreateNormalProbe(byte channel,
+                                                           double P_STIMULATE,
+                                                           double T_ON,
+                                                           double T_RESPONSE)
+        {
+            return new SetWaveformProgram()
+            {
+                Channel = channel,
+                Instructions = new SetWaveformProgram.Instruction[]
+                {
+                    new SetWaveformProgram.Instruction()
+                    {
+                        InstructionType = SetWaveformProgram.InstructionType.STEP,
+                        Argument = PressureToBinary(P_STIMULATE),
+                        Steps = TimeToCount(T_ON)
+                    },
+                    new SetWaveformProgram.Instruction(SetWaveformProgram.InstructionType.STEP)
+                    {
+                        Argument = 0,
+                        Steps = TimeToCount(T_RESPONSE)
+                    }
+                },
+                Repeat = 1
+            };
+        }
+
+        public static SetWaveformProgram CreateStartleProbe(byte channel,
+                                                            double P_STIMULATE,
+                                                            double P_STARTLE,
+                                                            double T_ON,
+                                                            double T_DELAY,
+                                                            double T_STARTLE,
+                                                            double T_RESPONSE)
+        {
+            List<SetWaveformProgram.Instruction> instr = new List<SetWaveformProgram.Instruction>();
+            ushort N_ON = TimeToCount(T_ON);
+            ushort N_DELAY = TimeToCount(T_DELAY);
+            ushort N_STARTLE = TimeToCount(T_STARTLE);
+
+            if ((N_DELAY > 0) && (N_DELAY + N_STARTLE < N_ON))
+            {
+                instr.Add(new SetWaveformProgram.Instruction(SetWaveformProgram.InstructionType.STEP)
+                {
+                    Argument = PressureToBinary(P_STIMULATE),
+                    Steps = N_DELAY
+                });
+                instr.Add(new SetWaveformProgram.Instruction(SetWaveformProgram.InstructionType.STEP)
+                {
+                    Argument = PressureToBinary(P_STARTLE),
+                    Steps = N_STARTLE
+                });
+                instr.Add(new SetWaveformProgram.Instruction(SetWaveformProgram.InstructionType.STEP)
+                {
+                    Argument = PressureToBinary(P_STIMULATE),
+                    Steps = (ushort)(N_ON - N_DELAY - N_STARTLE)
+                });
+            }
+            else if ((N_DELAY > 0) && (N_DELAY + N_STARTLE >= N_ON))
+            {
+                instr.Add(new SetWaveformProgram.Instruction(SetWaveformProgram.InstructionType.STEP)
+                {
+                    Argument = PressureToBinary(P_STIMULATE),
+                    Steps = N_DELAY
+                });
+                instr.Add(new SetWaveformProgram.Instruction(SetWaveformProgram.InstructionType.STEP)
+                {
+                    Argument = PressureToBinary(P_STARTLE),
+                    Steps = (ushort)(N_ON - N_DELAY)
+                });
+            }
+            else // ((N_DELAY == 0)
+            {
+                instr.Add(new SetWaveformProgram.Instruction(SetWaveformProgram.InstructionType.STEP)
+                {
+                    Argument = PressureToBinary(P_STARTLE),
+                    Steps = N_STARTLE
+                });
+                instr.Add(new SetWaveformProgram.Instruction(SetWaveformProgram.InstructionType.STEP)
+                {
+                    Argument = PressureToBinary(P_STIMULATE),
+                    Steps = (ushort)(N_ON - N_STARTLE)
+                });
+            }
+
+            instr.Add(new SetWaveformProgram.Instruction(SetWaveformProgram.InstructionType.STEP)
+            {
+                Argument = 0,
+                Steps = TimeToCount(T_RESPONSE)
+            });
+
+            return new SetWaveformProgram()
+            {
+                Channel = channel,
+                Instructions = instr.ToArray(),
+                Repeat = 1
+            };
+        }
+        #endregion
+
+    }
+}
---
+++ LabBench.CPAR/LabBench.CPAR/Functions/SetWaveformProgram.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Xml.Serialization;
+using Inventors.ECP;
+using Inventors.ECP.Communication;
+using Inventors.ECP.Utility;
+
+namespace Inventors.CPAR.Functions
+{
+    public class SetWaveformProgram :
+        DeviceFunction
+    {
+        public const int MAX_NO_OF_INSTRUCTIONS = 20;
+        public const int INSTRUCTIONS_LENGTH = 5;
+        public const byte FUNCTION_CODE = 0x12;
+        public const double MAX_PRESSURE = 100;
+        public const double UPDATE_RATE = 20;
+
+        public enum InstructionType
+        {
+            NOP = 0x00,
+            INC = 0x01,
+            DEC = 0x02,
+            STEP = 0x03
+        }
+
+        public class Instruction
+        {
+            private const double UPDATE_RATE = 20;
+            private const double MAX_PRESSURE = 100;
+
+            public Instruction()
+            {
+                encoding = new byte[INSTRUCTIONS_LENGTH];
+                InstructionType = InstructionType.NOP;
+                Argument = 0;
+                Steps = 1;
+            }
+
+            public Instruction(InstructionType type)
+            {
+                encoding = new byte[INSTRUCTIONS_LENGTH];
+                InstructionType = type;
+                Argument = 0;
+                Steps = 1;
+            }
+
+            [Category("Instruction")]
+            [XmlAttribute("instruction-type")]
+            public InstructionType InstructionType
+            {
+                get
+                {
+                    return (InstructionType)encoding[0];
+                }
+                set
+                {
+                    encoding[0] = (byte)value;
+                }
+            }
+
+            [Category("Instruction")]
+            [XmlAttribute("argument")]
+            public double Argument
+            {
+                get
+                {
+                    return (((double)encoding[1]) / 256) + encoding[2];
+                }
+                set
+                {
+                    double truncated;
+                    truncated = value > 255 ? 255 : value;
+                    truncated = value < 0 ? 0 : value;
+                    encoding[2] = (byte) Math.Truncate(truncated);
+                    encoding[1] = (byte) Math.Truncate(256*truncated);
+                }
+            }
+
+            [Category("Instruction")]
+            [XmlAttribute("steps")]
+            public ushort Steps
+            {
+                get
+                {
+                    return (ushort)(encoding[3] + encoding[4] * 256);
+                }
+                set
+                {
+                    ushort steps = (ushort) (value == 0 ? 1 : value);
+                    encoding[4] = (byte)(steps >> 8);
+                    encoding[3] = (byte)(steps - (ushort)(encoding[4] << 8));
+                }
+            }
+
+            [Category("Encoding")]
+            public byte[] Encoding
+            {
+                get
+                {
+                    return encoding;
+                }
+            }
+
+            public override string ToString()
+            {
+                string retValue = "Instruction";
+
+                switch (InstructionType)
+                {
+                    case InstructionType.INC:
+                        retValue = String.Format("{0} ({1:0.000}kPa/s, {2:0.00}s)",
+                                                  InstructionType.ToString(),
+                                                  MAX_PRESSURE*Argument/255,
+                                                  ((double)Steps)/UPDATE_RATE);
+                        break;
+                    case InstructionType.DEC:
+                        retValue = String.Format("{0} (-{1:0.000}kPa/s, {2:0.00}s)",
+                                                  InstructionType.ToString(),
+                                                  MAX_PRESSURE*Argument/255,
+                                                  ((double)Steps)/UPDATE_RATE);
+                        break;
+                    case InstructionType.STEP:
+                        retValue = String.Format("{0} ({1:0.000}kPa, {2:0.00}s)",
+                                                  InstructionType.ToString(),
+                                                  MAX_PRESSURE*Argument/255,
+                                                  ((double)Steps)/UPDATE_RATE);
+                        break;
+                    case InstructionType.NOP:
+                        retValue = String.Format("{0} ({1:0.00}s)",
+                                                  InstructionType.ToString(),
+                                                  ((double)Steps)/UPDATE_RATE); ;
+                        break;
+
+                }
+
+                return retValue;
+            }
+
+            private byte[] encoding;
+        }
+
+        public static Instruction CreateIncrementInstr(double delta, double time)
+        {
+            return new Instruction()
+            {
+                InstructionType = InstructionType.INC,
+                Argument = (255 * (delta / 100)) / UPDATE_RATE,
+                Steps = (ushort)(time * UPDATE_RATE)
+            };
+        }
+
+        public static Instruction CreateDecrementInstr(double delta, double time)
+        {
+            return new Instruction()
+            {
+                InstructionType = InstructionType.DEC,
+                Argument = (255 * (delta / 100)) / UPDATE_RATE,
+                Steps = (ushort)(time * UPDATE_RATE)
+            };
+        }
+
+        public static Instruction CreateStepInstr(double pressure, double time)
+        {
+            return new Instruction()
+            {
+                InstructionType = InstructionType.STEP,
+                Argument = (255 * (pressure / 100)),
+                Steps = (ushort)(time * UPDATE_RATE)
+            };
+        }
+
+        public SetWaveformProgram() : 
+            base(FUNCTION_CODE, requestLength: 0, responseLength: 1)
+        {
+        }
+
+        public override FunctionDispatcher CreateDispatcher() => new FunctionDispatcher(FUNCTION_CODE, () => new SetWaveformProgram());
+
+        public override bool Dispatch(dynamic listener) => listener.Accept(this);
+
+        [Category("Waveform Program")]
+        [XmlElement("instruction")]
+        public Instruction[] Instructions { get; set; } = new Instruction[0];
+
+        [Category("Waveform Program")]
+        [XmlAttribute("channel")]
+        public byte Channel
+        {
+            get
+            {
+                return channel;
+            }
+            set
+            {
+                channel = (byte) (value > 1 ? 1 : value);
+            }
+        }
+
+        [Category("Waveform Program")]
+        [XmlAttribute("repeat")]
+        public byte Repeat
+        {
+            get
+            {
+                return repeat;
+            }
+            set
+            {
+                repeat = (byte) (value > 0 ? value : 1);
+            }
+        }
+
+        [Category("Waveform Program")]
+        public byte ExpectedChecksum
+        {
+            get
+            {
+                return CRC8CCITT.Calculate(SerializeInstructions());
+            }
+        }
+
+        [Category("Waveform Program")]
+        public double ProgramLength
+        {
+            get
+            {
+                double retValue = 0;
+
+                foreach (var instr in Instructions)
+                {
+                    retValue += instr.Steps;
+                }
+
+                return retValue = Repeat * retValue/UPDATE_RATE;
+            }
+        }
+
+        [Category("Control")]
+        public byte ActualChecksum
+        {
+            get
+            {
+                if (Response != null)
+                {
+                    return Response.GetByte(0);
+                }
+                else
+                    return 0;
+            }
+        }
+
+        public override void OnSend()
+        {
+            var encodedInstructions = SerializeInstructions();
+            Request = new Packet(FUNCTION_CODE, encodedInstructions.Length + 2);
+            Request.InsertByte(0, Channel);
+            Request.InsertByte(1, Repeat);
+
+            for (int n = 0; n < encodedInstructions.Length; ++n)
+            {
+                Request.InsertByte(n + 2, encodedInstructions[n]);
+            }
+        }
+
+        private byte[] SerializeInstructions()
+        {
+            int noOfInstructions = NumberOfInstructions;
+            int numBytes = noOfInstructions * INSTRUCTIONS_LENGTH;
+            int counter = 0;
+            byte[] retValue = new byte[numBytes];
+
+            for (int n = 0; n < noOfInstructions; ++n)
+            {
+                foreach (var b in Instructions[n].Encoding)
+                {
+                    retValue[counter] = b;
+                    ++counter;
+                }
+            }
+
+            return retValue;
+        }
+
+        [Category("Waveform Program")]
+        public int NumberOfInstructions
+        {
+            get
+            {
+                return Instructions.Length > MAX_NO_OF_INSTRUCTIONS ?
+                       MAX_NO_OF_INSTRUCTIONS :
+                       Instructions.Length;
+            }
+        }
+
+        public override string ToString() => "[0x12] Set Waveform Program";
+
+        private byte channel = 0;
+        private byte repeat = 1;
+    }
+}
