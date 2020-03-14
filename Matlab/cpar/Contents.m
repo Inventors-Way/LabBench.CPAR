b34d09f6eb08072910209069d3a8d5774ba98bfc
Kristian Hennings <krhe@inventors.dk>
2020-03-14 09:01:21 +0100

    Update Contents.m

--- Matlab/cpar/Contents.m
+++ Matlab/cpar/Contents.m
@@ -1,7 +1,18 @@
 % Cuff Pressure Algometry Reseach (CPAR) Toolbox
 %
 % Files
-%   cparCreate - Create a CPAR Device.
-%   cparOpen   - Open the communication with a CPAR Device.
-%   cparClose  - Close the communication wht a CPAR Device.
-%   cparPing   - Ping a CPAR device.
+%   cparCreate         - Create a CPAR device
+%   cparOpen           - Open the communication with a CPAR device
+%   cparClose          - Close the communication wht a CPAR device
+%   cparPing           - Ping a CPAR device
+%   cparStart          - Start a CPAR device
+%   cparSetStimulus    - Set the stimulus for a pressure channel
+%   cparStop           - Stop pressure generation before it is completed
+%   cparPulse          - Create a rectangular waveform
+%   cparRamp           - Create a ramp waveform
+%   cparCombined       - Combine waveforms into a superimposed waveform
+%   cparCreateStimulus - Create a pressure stimulus
+%   cparIsUpdated      - Check if there is new data available
+%   cparGetData        - Get data from a CPAR device
+%   cparPlot           - Plot the data from a CPAR device
+%   cparVerbose        - Enable/disable diagnostic output
