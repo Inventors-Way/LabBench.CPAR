f112766a08d687e256b721b75d45c54e09a49691
Kristian Hennings <krhe@inventors.dk>
2020-03-16 18:47:23 +0100

    Added LabBench Tester

--- Matlab/example.m
+++ Matlab/example.m
@@ -2,33 +2,37 @@
 dev = cparCreate('COM18');
 cparOpen(dev);
 
-% Create a stimulus that can be used for temporal summation
-pon = cparPulse(50, 2, 0);
-poff = cparPulse(0, 4, 2);
-combined = cparCombined();
-cparCombinedAdd(combined, pon);
-cparCombinedAdd(combined, poff);
-stimulus = cparCreateStimulus(1, 5, combined);
+try
+    % Create a stimulus that can be used for temporal summation
+    pon = cparRamp(50, 1, 0);
+    pon2 = cparPulse(50, 1, 1);
+    poff = cparPulse(20, 2, 1.5);
+    combined = cparCombined();
+    cparCombinedAdd(combined, pon);
+    cparCombinedAdd(combined, pon2);
+    cparCombinedAdd(combined, poff);
 
-% Update the device with the created stimulus
-cparSetStimulus(dev, stimulus);
+    % Update the device with the created stimulus
+    cparSetStimulus(dev, cparCreateStimulus(1, 1, combined));
 
-% Make sure the other channel is set to zero.
-cparSetStimulus(dev, cparCreateStimulus(2, 1, cparPulse(0, 0.1, 0))); 
+    % Make sure the other channel is set to zero.
+    cparSetStimulus(dev, cparCreateStimulus(2, 1, cparPulse(0, 0.1, 0))); 
 
-% Start the stimulation
-cparStart(dev, 'b', true);
+    % Start the stimulation
+    cparStart(dev, 'b', true);
 
-% Wait until stimulation has completed
-pause(1);
-while (dev.State == LabBench.Interface.AlgometerState.STATE_STIMULATING)
-    fprintf('State: %s\n', dev.State.ToString()); 
+    % Wait until stimulation has completed
     pause(1);
+    while (dev.State == LabBench.Interface.AlgometerState.STATE_STIMULATING)
+        fprintf('State: %s\n', dev.State.ToString()); 
+        pause(1);
+    end
+    fprintf('State: %s\n', dev.State.ToString()); 
+
+    % Retrive data and plot it.
+    data = cparGetData(dev);
+    cparPlot(data);
+    cparClose(dev);
+catch
+   cparClose(dev); 
 end
-fprintf('State: %s\n', dev.State.ToString()); 
-
-% Retrive data and plot it.
-data = cparGetData(dev);
-cparPlot(data);
-cparClose(dev);
-
