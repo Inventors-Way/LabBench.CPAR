44d374e1c77e387d0158ee2493e4195dd4a874da
Kristian Hennings <krhe@inventors.dk>
2021-03-26 16:02:15 +0100

    Update cparIsReady.m

--- cpar/cparIsReady.m
+++ cpar/cparIsReady.m
@@ -1,5 +1,9 @@
 function [ready] = cparIsReady(dev)
 % cparIsReady Is the device ready to perform a pressure stimulation
+%   [ready] = cparIsReady(dev) checks if a cpar device [dev] is ready to
+%   perform a pressure stimulation.
+%
+% See also, cparGetAdvice
 
 if dev.Ready
     ready = 1;
