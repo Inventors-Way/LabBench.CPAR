1ee979be601cbcab55ffff29e2f1339ae20b119d
Kristian Hennings <krhe@inventors.dk>
2021-03-26 15:59:45 +0100

    Update cparPing.m

--- cpar/cparPing.m
+++ cpar/cparPing.m
@@ -1,6 +1,10 @@
 function cparPing(dev, enable)
 % cparPing Ping a CPAR device.
-%   cparPing(dev, enable)
+%   cparPing(dev, enable) enables ping of the cpar device if [enable] is
+%   set to 1. If ping is enabled the connection is also checked and the
+%   current ping count is displayed.
+%
+% See also, cparGetDevice
 
 if enable
     try    
