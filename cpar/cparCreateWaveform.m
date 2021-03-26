79559437325f070ca1a948108f7bb7b091b049aa
Kristian Hennings <krhe@inventors.dk>
2021-03-26 13:40:05 +0100

    Update cparCreateWaveform.m

--- cpar/cparCreateWaveform.m
+++ cpar/cparCreateWaveform.m
@@ -1,6 +1,13 @@
 function [waveform] = cparCreateWaveform(channel, repeat)
 % cparCreateWaveformProgram Create a waveform program
-%   [waveform] = cparCreateWaveformProgram(channel, repeat)
+%   [waveform] = cparCreateWaveformProgram(channel, repeat) this function
+%   creates an empty waveform for pressure outlet no. [channel] that will
+%   be repeated [repeat] number of times.
+%
+% Note:
+%
+%
+% See also, cparWaveform_Step, cparWaveform_Inc, cparWaveform_Dec
 waveform = LabBench.Instruments.CPAR.Functions.SetWaveformProgram;
 waveform.Channel = channel - 1;
 waveform.Repeat = repeat;
